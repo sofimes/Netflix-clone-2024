a480eded4b15f18ce644701914b895c80a0c14f0
sofimesfin <150360094+sofimes@users.noreply.github.com>
2024-05-08 15:42:03 +0300

    banner finished

--- netflix_clone/src/Utils/Axios.js
+++ netflix_clone/src/Utils/Axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const instance = axios.create({
-	baseURL: "https://api.themoviedb.org/",
+	baseUrl: "https://api.themoviedb.org/3",
 });
-export default { instance, axios };
+export default instance;
--- netflix_clone/src/Utils/Requests.js
+++ netflix_clone/src/Utils/Requests.js
@@ -1,7 +1,7 @@
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 const requests = {
 	fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-	fetchNetflixOrginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+	fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
 	fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
 	fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
 	fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
